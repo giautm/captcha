13e554c3155715e76770101aef52c89129e7d456
Giau. Tran Minh <giau.tmg@gmail.com>
2021-03-31 16:57:36 +0000

    fix: update error

--- fisheye/preprocessor.go
+++ fisheye/preprocessor.go
@@ -6,6 +6,10 @@ import (
 	"image"
 )
 
+var (
+	ErrDetectDistance = errors.New("fisheye: can not detect distance")
+)
+
 type FisheyePreprocessor struct{}
 
 func NewPreprocessor() *FisheyePreprocessor {
@@ -15,7 +19,7 @@ func NewPreprocessor() *FisheyePreprocessor {
 func (p *FisheyePreprocessor) Preprocess(_ context.Context, img image.Image) (image.Image, error) {
 	result, distance := FindDistance(img)
 	if distance < 0 {
-		return nil, errors.New("preprocess: can not detect distance")
+		return nil, ErrDetectDistance
 	}
 
 	return result, nil
